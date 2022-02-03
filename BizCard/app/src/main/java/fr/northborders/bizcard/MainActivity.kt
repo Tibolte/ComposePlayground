fc080a54df92d8b84910fc5f0f9df4955890580d
Thibault Guegan <thibault@travelloapp.com>
2022-02-03 18:37:32 +0100

    Add BizCard implementation

--- BizCard/app/src/main/java/fr/northborders/bizcard/MainActivity.kt
+++ BizCard/app/src/main/java/fr/northborders/bizcard/MainActivity.kt
@@ -3,13 +3,28 @@ package fr.northborders.bizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.northborders.bizcard.ui.theme.BizCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BizCard()
                 }
             }
         }
@@ -30,14 +45,151 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BizCard() {
+    val btnClickState = remember {
+        mutableStateOf(false)
+    }
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp
+        ) {
+
+            Column(
+                modifier = Modifier.height(300.dp),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ImageProfile()
+                Divider(thickness = 7.dp)
+                Info()
+                Button(onClick = {
+                    btnClickState.value = !btnClickState.value
+                }) {
+                    Text(
+                        text = "Porfolio",
+                        color = Color.White,
+                        style = MaterialTheme.typography.body1
+                    )
+                }
+                if (btnClickState.value) {
+                    Content()
+                } else {
+                    Box() {}
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Info() {
+    Column(
+        modifier = Modifier.padding(5.dp),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "Miles P.",
+            color = Color.Blue,
+            style = MaterialTheme.typography.h4
+        )
+        Text(
+            modifier = Modifier.padding(5.dp),
+            text = "Android Compose Programmer",
+            color = Color.Black,
+            style = MaterialTheme.typography.body1
+        )
+        Text(
+            modifier = Modifier.padding(5.dp),
+            text = "@themilesCompose",
+            color = Color.Black,
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+private fun ImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.android),
+            contentDescription = "Profile Image",
+            modifier = modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Preview
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(3.dp),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+            Porfolio(listOf("Project 1", "Project 2",
+                "Project 3", "Project 4", "Project 5"))
+        }
+    }
+}
+
+@Composable
+fun Porfolio(list: List<String>) {
+    LazyColumn {
+        items(list) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(13.dp)
+                    .fillMaxWidth(),
+                shape = RectangleShape
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(MaterialTheme.colors.surface)
+                        .padding(7.dp)
+                ) {
+                    ImageProfile(Modifier.size(100.dp))
+                    Column(modifier = Modifier.padding(8.dp).align(Alignment.CenterVertically)) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great project", style = MaterialTheme.typography.body2)
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BizCardTheme {
-        Greeting("Android")
+        BizCard()
     }
 }
